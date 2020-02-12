9517a9c7ec530961b51ea02e2af5da7a9f7b8cd5
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-02-12 13:16:31 +0100

    add method index show and new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+olivier = Restaurant.new(name: "L'Olivier", address: "rue ordener, Paris", category: "french", phone_number: "0256781928")
+olivier.save
+souris = Restaurant.new(name: "La Souris Verte", address: "rue marcadet, Paris", category: "french", phone_number: "07902490924")
+souris.save
+hokkaido = Restaurant.new(name: "hokkaido", address: "rue lamarck, Paris", category: "japanese", phone_number: "33171244014")
+hokkaido.save
+leon = Restaurant.new(name: "Chez Leon", address: "square Marguerite, Bruxelles", category: "belgian", phone_number: "345782392194")
+leon.save
+pulcinella = Restaurant.new(name: "Pulcinella", address: "via rimini, Rome", category: "italian", phone_number: "4783924689023")
+pulcinella.save
